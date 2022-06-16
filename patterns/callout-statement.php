dafec7089b2bf69b2c1e3d01149f8d82cff3ba32
Tim Lindgren <contact@timlindgren.com>
2022-06-16 18:57:27 -0400

    Adding colors and statement callout pattern

---
+++ patterns/callout-statement.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Callout - Opportunity Statement
+ * Slug: cdil/callout-statement
+ * Categories: cdilblocks-callouts
+ * Block Types: core/pullquote
+ * Viewport Width: 1280
+ */
+
+?>
+<!-- wp:pullquote {"style":{"border":{"radius":"3px","width":"0px","style":"none"}},"backgroundColor":"light-warm-gray","className":"is-style-shadow"} -->
+<figure class="wp-block-pullquote is-style-shadow has-light-warm-gray-background-color has-background" style="border-radius:3px;border-style:none;border-width:0px"><blockquote><p>[Statement Text]</p></blockquote></figure>
+<!-- /wp:pullquote -->
