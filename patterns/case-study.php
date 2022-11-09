b866721f77790530234f2543fef24480ccf5431e
Tim Lindgren <contact@timlindgren.com>
2022-11-09 11:22:44 -0500

    Updating after theme export

---
+++ patterns/case-study.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Title:       Case Study Template
+ * Slug:        cidblocks/casestudy
+ * Categories:  cdilblocks-casestudy
+ * postTypes: reresource
+ * blockTypes:  core/post-content
+?>
+
+<!-- wp:group {"align":"full","layout":{"inherit":true},"style":{"spacing":{"padding":{"bottom":"var(--wp--style--block-gap)","top":"var(--wp--style--block-gap)"}}}} -->
+<div class="wp-block-group alignfull" style="padding-bottom:var(--wp--style--block-gap);padding-top:var(--wp--style--block-gap)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:site-logo {"width":64} /-->
+
+<!-- wp:group -->
+<div class="wp-block-group"><!-- wp:site-title /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">© <?php echo gmdate( 'Y' ); ?> Your Company LLC</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:social-links {"iconColor":"foreground","iconColorValue":"var(--wp--preset--color--foreground)","size":"has-small-icon-size","className":"is-style-logos-only","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"},"style":{"spacing":{"blockGap":"1rem"}}} -->
+<ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"#","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"#","service":"twitter"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+<?php
+/**
+ * Title:       Case Study
+ * Slug:        cidlblocks/casestudy
+ * Categories:  pages
+ * postTypes:   core/post-content
+ */
+?>
+
+<!-- wp:paragraph {"backgroundColor":"Info"} -->
+<p class="has-info-background-color has-background">[Succinct, easy to digest summary of the the case study]</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":1029,"sizeSlug":"full","linkDestination":"none","className":"is-style-editorskit-shadow"} -->
+<figure class="wp-block-image size-full is-style-editorskit-shadow"><img src="https://dev-cdil-resources.pantheonsite.io/wp-content/uploads/2022/07/placeholder.png" alt="" class="wp-image-1029"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading -->
+<h2>Context</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading -->
+<h2>Approach</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading -->
+<h2>Outcomes</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading -->
+<h2>Resources</h2>
+<!-- /wp:heading -->
