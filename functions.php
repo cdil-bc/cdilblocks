29249eec52fb38f9d4529d448dfe9f3d16d8f706
Tim Lindgren <timothy.lindgren@bc.edu>
2022-11-13 22:27:51 -0500

    Removing default logo

--- functions.php
+++ functions.php
@@ -302,12 +302,3 @@ function showAdminMessages() {
 		echo '</div>';
 	}
 }
-
-// Set Default Logo
-$custom_logo_id = get_theme_mod( 'custom_logo' );
-$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
-if ( has_custom_logo() ) {
-        echo '<img src="'. esc_url( $logo[0] ) .'">';
-} else {
-        echo '<img src="'. get_stylesheet_directory_uri() . '/assets/images/BC-Wordmark-Single-Line-Gold.png' .'">';
-}
