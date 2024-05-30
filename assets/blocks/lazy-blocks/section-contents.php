a03ca53eaef89db20ea11932567a73bfc42aad06
Tim Lindgren <timothy.lindgren@bc.edu>
2024-05-30 13:38:31 -0400

    Revised version, based on sub-children version

--- assets/blocks/lazy-blocks/section-contents.php
+++ assets/blocks/lazy-blocks/section-contents.php
@@ -1,43 +1,39 @@
 <?php
 global $post;
-$args = array(
-    'post_type'      => 'page',
-    'posts_per_page' => -1,
-    'post_parent'    => $post->ID,
-    'order'          => 'ASC',
-    'orderby'        => 'menu_order'
- ); 
-
-$parent = new WP_Query( $args );
-
-
+// Get the children of the current page
+$children = get_pages(array(
+    'parent' => $post->ID,
+    'sort_column' => 'menu_order',
+    'sort_order' => 'ASC'
+));
 ?>
 <nav class="section-contents">
-<ul class="parent-page">
+<?php if (!empty($children)): ?>
+    <ul class="parent-page">
+        <?php foreach ($children as $child): ?>
+            <li>
+                <a href="<?php echo get_permalink($child->ID); ?>" title="<?php echo $child->post_title; ?>">
+                    <?php echo $child->post_title; ?>
+                </a>
+                <?php
+                // Fetch the excerpt for the child page
+                $child_post = get_post($child->ID);
+                $excerpt = $child_post->post_excerpt;
+                if ($excerpt): ?>
+                    <p class="entry-subtitle"><?php echo $excerpt; ?></p>
+                <?php endif; ?>
 
-<?php if ( $parent->have_posts() ) : ?>
-    <?php while ( $parent->have_posts() ) : $parent->the_post(); 
-    $children = get_pages('child_of='.$post->ID);
-    $count = count($children);?>    
-    <li><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-      <?php if( count( $children ) != 0 ): ?>
-        <span class="child-count"> <?php echo $count ?></span>
-       <?php endif; ?>
-      <?php if ( ! has_excerpt() ) :
-      echo '';  
-      else: ?>
-        <p class="entry-subtitle"><?php echo get_the_excerpt(); ?></p>
-      <?php endif; ?></li>
-    <?php endwhile; ?>
+                <?php
+                // Get the first level of subchildren for the current child page
+                $subchildren = get_pages(array(
+                    'parent' => $child->ID,
+                    'sort_column' => 'menu_order',
+                    'sort_order' => 'ASC'
+                ));
+                ?>
 
-<?php endif; wp_reset_postdata(); ?>
-
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
 </nav>
-</ul>
-<?php
-function new_excerpt_more( $more ) {
-    return '';
-}
-add_filter('excerpt_more', 'new_excerpt_more');
-
-?>
